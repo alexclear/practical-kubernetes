585a81a8e41a32771cabd4a82d833e1738a53922
Alex Chistyakov <alexclear@gmail.com>
2023-07-06 21:11:32 +0300

    Make the worker node provisioning script "vagrant provision"-ready

--- scripts/node.sh
+++ scripts/node.sh
@@ -5,14 +5,20 @@
 set -euxo pipefail
 
 config_path="/vagrant/configs"
-
-/bin/bash $config_path/join.sh -v
+NODENAME=$(hostname -s)
 
 sudo -i -u vagrant bash << EOF
 whoami
 mkdir -p /home/vagrant/.kube
 sudo cp -i $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
-NODENAME=$(hostname -s)
-kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
+EOF
+
+# Check if the node is already part of a Kubernetes cluster
+if ! sudo -i -u vagrant kubectl get nodes | grep -q $NODENAME; then
+    /bin/bash $config_path/join.sh -v
+fi
+
+sudo -i -u vagrant bash << EOF
+kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker --overwrite
 EOF
