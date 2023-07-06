e62f9f62eb2a6249296c12b9f59aebb9649791f5
Alex Chistyakov <alexclear@gmail.com>
2023-07-06 22:42:57 +0300

    Create the ArgoCD project called "infrastructure"

--- scripts/argocd.sh
+++ scripts/argocd.sh
@@ -2,3 +2,5 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm upgrade --install -n argocd argocd argo/argo-cd --create-namespace -f applications-helm/argocd/argocd/values.yaml --version=5.33.3
+kubectl apply -f applications-raw/argocd/project-infrastructure/appproject.yaml -n argocd
+kubectl apply -f main-application.yaml -n argocd
